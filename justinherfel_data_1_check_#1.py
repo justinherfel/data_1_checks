d355b46aec4b234359f74c281fe07a9b86a17f4e
Justin Herfel <justinherfel@yahoo.com>
2022-05-21 11:44:46 -0400

    print one value from a list

--- justinherfel_data_1_check_#1.py
+++ justinherfel_data_1_check_#1.py
@@ -1,1 +1,5 @@
 print('hello world!')
+
+top_ten_movies_qt_list = ['The Good, the Bad and the Ugly', 'Rio Bravo', 'Blow Out', 'Taxi Driver', 'His Girl Friday', 'Five Fingers of Death', 'Pandora\'s Box', 'Carrie', 'Unfaithfully Yours', 'Five Graves of Cairo', 'Jaws']
+print(top_ten_movies_qt_list[0])
+
