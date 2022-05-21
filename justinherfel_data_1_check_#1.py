3fc20376dba9a37e46d41169232e3c377dc5ed92
Justin Herfel <justinherfel@yahoo.com>
2022-05-21 11:56:23 -0400

    print one value from dict

--- justinherfel_data_1_check_#1.py
+++ justinherfel_data_1_check_#1.py
@@ -3,3 +3,5 @@ print('hello world!')
 top_ten_movies_qt_list = ['The Good, the Bad and the Ugly', 'Rio Bravo', 'Blow Out', 'Taxi Driver', 'His Girl Friday', 'Five Fingers of Death', 'Pandora\'s Box', 'Carrie', 'Unfaithfully Yours', 'Five Graves of Cairo', 'Jaws']
 print(top_ten_movies_qt_list[0])
 
+top_ten_movies_qt_dict = {'The Good, The Bad and the Ugly': 'Sergio Leone - Dir', 'Rio Bravo': 'Howard Hawks - Dir'}
+print(top_ten_movies_qt_dict['The Good, The Bad and the Ugly'])
